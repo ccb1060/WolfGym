8f2aee45b81925eff855731ec30ddc47358d5e9b
rrh3969 <rrh3969@rit.edu>
2024-11-16 11:21:38 -0500

    buzzword manager

--- The Collection of Individuals/Assets/Scripts/BuzzwordsManager.cs
+++ The Collection of Individuals/Assets/Scripts/BuzzwordsManager.cs
@@ -1,5 +1,8 @@
+using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 
@@ -8,14 +11,27 @@ public class BuzzwordsManager : MonoBehaviour
     //dictionaries to store words and connection values
     private Dictionary<string, int> Topical = new Dictionary<string, int>();
     private Dictionary<string, int> Generic = new Dictionary<string, int>();
+    StreamReader TopicalReader;
+    StreamReader GenericReader;
 
     private void Start()
     {
         //intialize topical
-
-
+        TopicalReader = new StreamReader("Assets\\TextFiles\\Topical.txt");
+        string topicalContent = TopicalReader.ReadToEnd();
+        string[] topicalWords = topicalContent.Split(",");
+        foreach(string word in topicalWords)
+        {
+            Topical.Add(word, -1);
+        }
         //intialize generic
-
+        GenericReader = new StreamReader("Assets\\TextFiles\\Generic.txt");
+        string genericContent = GenericReader.ReadToEnd();
+        string[] genericWords = genericContent.Split(",");
+        foreach (string word in genericWords)
+        {
+            Generic.Add(word, 1);
+        }
     }
 
     //reset topical scores
@@ -32,8 +48,8 @@ public class BuzzwordsManager : MonoBehaviour
     public string PickTopical()
     {
         string temp = "";
-        temp = Topical.Keys.ToArray()[(int)Random.Range(0, Topical.Keys.Count - 1)];
-        Topical[temp] = 5;
+        temp = Topical.Keys.ToArray()[(int)UnityEngine.Random.Range(0, Topical.Keys.Count - 1)];
+        Topical[temp] = 3;
         return temp;
     }
 
