a98833f47caba7f5063f6f38767d66d3e6a3f3d0
rrh3969 <rrh3969@rit.edu>
2024-11-16 18:58:01 -0500

    scoring fix

--- The Collection of Individuals/Assets/Scripts/NewsManager.cs
+++ The Collection of Individuals/Assets/Scripts/NewsManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 using UnityEngine.UIElements;
 using System.IO;
 using TMPro;
+using UnityEngine.Windows;
 
 public class NewsManager : MonoBehaviour
 {
@@ -18,6 +19,8 @@ public class NewsManager : MonoBehaviour
     //The field that displays the text
     [SerializeField] TMP_Text postText;
 
+    [SerializeField] CanvasRandomizer canvas;
+
     //All possible articles
     private string[] articleList;
 
@@ -26,6 +29,7 @@ public class NewsManager : MonoBehaviour
         get { return articleList; }
     }
 
+    List<string> inputWords = new List<string>();
 
     //the current post being worked on
     string[] post;
@@ -46,6 +50,13 @@ public class NewsManager : MonoBehaviour
     public void changeArticle()
     {
         //Picks a random article from the list, rerolling if it gets the same one
+        foreach(string word in inputWords)
+        {
+            gameManager.playerScore += buzzwordsManager.GetScore(word);
+            canvas.IncreaseConnections(buzzwordsManager.GetScore(word));
+            buzzwordsManager.Discover(word);
+        }
+        inputWords.Clear();
         post = articleList[Random.Range(1, articleList.Length - 1)].Split(';');
         buzzwordsManager.PickTopic();
         updatePost("");
@@ -61,8 +72,7 @@ public class NewsManager : MonoBehaviour
                 bodyWhole += "<color=red>_</color>" + bodyParts[i];
             else
             {
-                gameManager.playerScore += buzzwordsManager.GetScore(input);
-                buzzwordsManager.Discover(input);
+                inputWords.Add(input);
                 bodyWhole += "<color=purple>" + input + "</color>"+bodyParts[i];
                 input = "";
             }
