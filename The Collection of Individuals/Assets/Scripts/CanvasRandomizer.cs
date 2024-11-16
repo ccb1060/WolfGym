2a08cd103dd8162e028b30e89c1f9e5f8bc008f3
AndrewJameison <awjameison@gmail.com>
2024-11-16 17:13:47 -0500

    Finished CanvasRandomizer
    Funny numbers go up randomly in proportion to num of connections

--- The Collection of Individuals/Assets/Scripts/CanvasRandomizer.cs
+++ The Collection of Individuals/Assets/Scripts/CanvasRandomizer.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using TMPro;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class CanvasRandomizer : MonoBehaviour
 {
@@ -12,45 +15,70 @@ public class CanvasRandomizer : MonoBehaviour
 
     public int connections { get; private set; } = 0;
 
+    void Start()
+    {
+        _connections.text = "Virtual Content Marketing Response intern @ BigMedia\n" +
+                            "Connections: 0";
+        _likes.text = "0";
+        _comments.text = "0";
+        _pop_hashtags.text = Random.Range(2.0f, 50.0f).ToString("0.0") + "M New Posts!";
+    }
+
+    /// <summary>
+    /// Formats and randomizes a collection of meaningless number values on screen.
+    /// Should be called every time the player gains new connections.
+    /// </summary>
+    /// <param name="newConnections"></param>
     public void IncreaseConnections(int newConnections)
     {
         connections += newConnections;
 
-        float likes = connections;
-        float comments = connections;
-        float pop = connections;
+        float likes = connections * Random.Range(0.4f, 0.8f);
+        float comments = connections * Random.Range(0.05f, 0.5f);
+        float pop = Random.Range(2.0f, 50.0f);
 
-        _connections.text = FormatBigNumbers(connections);
-        _likes.text = FormatBigNumbers(connections);
-        _comments.text = FormatBigNumbers(connections);
-        _pop_hashtags.text = pop + "M New Posts!";
+        _connections.text = "Media Consultant Intern @ BigMedia\n" +
+                            "Connections: " + FormatBigNumbers(connections);
+
+        _likes.text = FormatBigNumbers(likes);
+        _comments.text = FormatBigNumbers(comments);
+        _pop_hashtags.text = pop.ToString("0.0") + "M New Posts!";
     }
 
     /// <summary>
-    /// 
+    /// Turns really big numbers into equivalent truncated strings
     /// </summary>
     /// <param name="num"></param>
     /// <returns></returns>
     private string FormatBigNumbers(float num)
     {
         string character = "";
+        string value = num.ToString("0");
 
-        num /= 1000;
-
-        if (num < 1)
+        if (num >= 1000000000) // Billion
         {
-
+            character = "B";
+            value = (num / 1000000000).ToString("0.0");
         }
-        else if (num > 1) 
-        { 
-        
+        else if (num >= 1000000) // Million
+        {
+            character = "M";
+            value = (num / 1000000).ToString("0.0");
         }
-
-
-
-        
-        string value = (num %= 1000).ToString();
+        else if (num >= 1000) // Thousand
+        {
+            character = "K";
+            value = (num / 1000).ToString("0.0");
+        }
 
         return value + character;
+    }
+
+    /// <summary>
+    /// A temporary button event to quit the game. Can be moved to whereever
+    /// </summary>
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
